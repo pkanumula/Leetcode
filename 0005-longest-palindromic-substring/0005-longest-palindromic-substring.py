bd2e083a1418c9607c023bce06c574483182d45a
Pavan Kalyan Anumula <143294074+pkanumula@users.noreply.github.com>
2024-07-22 10:55:32 -0400

    Time: 334 ms (76.72%), Space: 11.8 MB (33.66%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -4,33 +4,25 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        n = len(s)
-        if n == 0:
+        if not s:
             return ""
 
-        # Initialize a table to store the palindrome status
-        dp = [[False] * n for _ in range(n)]
-        start = 0
-        max_length = 1
+        def expand_around_center(left, right):
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return s[left + 1:right]
 
-        # All substrings of length 1 are palindromes
-        for i in range(n):
-            dp[i][i] = True
+        longest = ""
+        for i in range(len(s)):
+            # Check for odd-length palindromes (single center)
+            palindrome1 = expand_around_center(i, i)
+            if len(palindrome1) > len(longest):
+                longest = palindrome1
 
-        # Check for sub-strings of length 2
-        for i in range(n - 1):
-            if s[i] == s[i + 1]:
-                dp[i][i + 1] = True
-                start = i
-                max_length = 2
+            # Check for even-length palindromes (double center)
+            palindrome2 = expand_around_center(i, i + 1)
+            if len(palindrome2) > len(longest):
+                longest = palindrome2
 
-        # Check for lengths greater than 2
-        for length in range(3, n + 1):
-            for i in range(n - length + 1):
-                j = i + length - 1
-                if s[i] == s[j] and dp[i + 1][j - 1]:
-                    dp[i][j] = True
-                    start = i
-                    max_length = length
-
-        return s[start:start + max_length]
+        return longest
