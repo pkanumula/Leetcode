66381f370e0595c3c71711905d3aec8e47052249
Pavan Kalyan Anumula <143294074+pkanumula@users.noreply.github.com>
2024-07-24 10:10:33 -0400

    Time: 20 ms (15.97%), Space: 11.6 MB (19.33%) - LeetHub

--- 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.py
+++ 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.py
@@ -1,4 +1,11 @@
 class Solution(object):
     def hasAlternatingBits(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        # XOR n with n >> 1
         xor = n ^ (n >> 1)
+        
+        # Check if xor is all 1s
         return (xor & (xor + 1)) == 0
