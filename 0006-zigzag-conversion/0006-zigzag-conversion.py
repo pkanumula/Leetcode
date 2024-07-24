ae9e3e5c1d097fbb6d1891212d6a897579302204
Pavan Kalyan Anumula <143294074+pkanumula@users.noreply.github.com>
2024-07-24 01:09:00 -0400

    Time: 27 ms (90.62%), Space: 11.8 MB (42.87%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -1,20 +1,25 @@
 class Solution(object):
     def convert(self, s, numRows):
-        # If numRows is 1, no zigzag pattern can be formed
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
         if numRows == 1 or numRows >= len(s):
             return s
         
-        # Create an array to hold the strings for each row
         rows = [''] * numRows
         current_row = 0
-        direction = -1
+        step = 1
         
         for char in s:
             rows[current_row] += char
-            # Change direction when we reach the top or bottom row
-            if current_row == 0 or current_row == numRows - 1:
-                direction *= -1
-            current_row += direction
+            
+            if current_row == 0:
+                step = 1
+            elif current_row == numRows - 1:
+                step = -1
+            
+            current_row += step
         
-        # Join all rows to get the final string
         return ''.join(rows)
