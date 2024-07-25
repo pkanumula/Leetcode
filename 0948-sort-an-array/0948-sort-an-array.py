09efb7d5354afa1eb9d6c652ab9f50b2c8ab67ee
Pavan Kalyan Anumula <143294074+pkanumula@users.noreply.github.com>
2024-07-25 00:04:34 -0400

    Time: 1437 ms (66.27%), Space: 20.7 MB (48.4%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.py
+++ 0948-sort-an-array/0948-sort-an-array.py
@@ -4,39 +4,29 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        def merge_sort(start, end):
-            if end - start <= 1:
-                return
-            
-            mid = (start + end) // 2
-            merge_sort(start, mid)
-            merge_sort(mid, end)
-            merge(start, mid, end)
-        
-        def merge(start, mid, end):
-            left = nums[start:mid]
-            right = nums[mid:end]
-            i = j = 0
-            k = start
-            
-            while i < len(left) and j < len(right):
-                if left[i] <= right[j]:
-                    nums[k] = left[i]
-                    i += 1
-                else:
-                    nums[k] = right[j]
-                    j += 1
-                k += 1
-            
-            while i < len(left):
-                nums[k] = left[i]
-                i += 1
-                k += 1
-            
-            while j < len(right):
-                nums[k] = right[j]
-                j += 1
-                k += 1
-        
-        merge_sort(0, len(nums))
-        return nums
+        if len(nums) <= 1:
+            return nums
+
+        mid = len(nums) // 2
+        left_half = self.sortArray(nums[:mid])
+        right_half = self.sortArray(nums[mid:])
+
+        return self.merge(left_half, right_half)
+
+    def merge(self, left, right):
+        merged = []
+        left_index = 0
+        right_index = 0
+
+        while left_index < len(left) and right_index < len(right):
+            if left[left_index] <= right[right_index]:
+                merged.append(left[left_index])
+                left_index += 1
+            else:
+                merged.append(right[right_index])
+                right_index += 1
+
+        merged.extend(left[left_index:])
+        merged.extend(right[right_index:])
+
+        return merged
