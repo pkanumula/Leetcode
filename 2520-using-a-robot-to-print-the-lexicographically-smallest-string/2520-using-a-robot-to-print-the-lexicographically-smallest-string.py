6649f35040ae263dd702d5737e5c733f7695f558
Pavan Kalyan Anumula <143294074+pkanumula@users.noreply.github.com>
2025-06-05 19:18:47 -0500

    Time: 487 ms (53.81%), Space: 22.8 MB (15.27%) - LeetHub

--- 2520-using-a-robot-to-print-the-lexicographically-smallest-string/2520-using-a-robot-to-print-the-lexicographically-smallest-string.py
+++ 2520-using-a-robot-to-print-the-lexicographically-smallest-string/2520-using-a-robot-to-print-the-lexicographically-smallest-string.py
@@ -1,24 +1,55 @@
+import collections
+
 class Solution:
-    def robotWithString(self, s: str) -> str:
-        from collections import deque
+  def robotWithString(self, s: str) -> str:
+    """
+    Finds the lexicographically smallest string that can be written on paper.
 
-        # Precompute suffix minimums
-        n = len(s)
-        min_suff = [''] * n
-        min_suff[-1] = s[-1]
-        for i in range(n - 2, -1, -1):
-            min_suff[i] = min(s[i], min_suff[i + 1])
+    This solution uses a greedy approach. At each step, it decides whether to
+    move a character from the input string `s` to a temporary stack `t`, or
+    from `t` to the result string `p`.
 
-        t = []  # Stack
-        result = []  # Final output string
-        for i in range(n):
-            t.append(s[i])
-            # Pop from t to result if top of t <= min of remaining s
-            while t and (i == n - 1 or t[-1] <= min_suff[i + 1]):
-                result.append(t.pop())
+    To make the optimal greedy choice, we precompute the minimum character for
+    every suffix of `s`. This allows us to compare the character at the top of
+    the stack with the smallest character we might encounter in the future from `s`.
 
-        # Any remaining in t can be popped now
-        while t:
-            result.append(t.pop())
+    The logic is as follows:
+    1. A character `t.top()` is moved to `p` only if it's less than or equal
+       to the minimum of all remaining characters in `s`.
+    2. Otherwise, we must move a character from `s` to `t` to get closer to
+       the future smallest character.
 
-        return ''.join(result)
+    This ensures that we always append the smallest possible character to the result
+    at any given time.
+    """
+    
+    # suffix_min[i] will store the minimum character in s[i:]
+    suffix_min = [''] * (len(s) + 1)
+    # Use a sentinel value larger than any lowercase letter
+    suffix_min[len(s)] = '~' 
+    
+    # Precompute the suffix minimums in O(n)
+    for i in range(len(s) - 1, -1, -1):
+      suffix_min[i] = min(s[i], suffix_min[i+1])
+      
+    # `t` acts as the robot's temporary string (stack)
+    t = [] 
+    # `p` will build the final result string
+    p = []
+    
+    # Iterate through the input string s
+    for i in range(len(s)):
+      # Operation 1: Move the character from s to t
+      t.append(s[i])
+      
+      # Operation 2: Greedily move characters from t to p
+      # We pop from t if its top character is less than or equal to the
+      # minimum character in the rest of s (s[i+1:])
+      while t and t[-1] <= suffix_min[i+1]:
+        p.append(t.pop())
+        
+    # Append any remaining characters from the stack t
+    while t:
+      p.append(t.pop())
+      
+    return "".join(p)
